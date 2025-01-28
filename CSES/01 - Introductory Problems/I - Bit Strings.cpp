05adae01685800238fddb6b1b1ce7d7f87ded794
Evelyn <evelynzanpx@gmail.com>
2025-01-28 14:24:18 +0700

    Task 1617 - Bit Strings

---
+++ CSES/01 - Introductory Problems/I - Bit Strings.cpp
@@ -0,0 +1,48 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+# define GRIND    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+# define db(x) cout << #x << " -> " << x << endl
+
+// Data Type
+# define int      long long
+
+// Constants
+const int INF      = 1e18;
+const int mod      = 1e9 + 7;
+const int mod_2    = 998244353;
+const int MAXN     = 100000;
+const int MAXN2    = 200000;
+
+// Pairs
+# define pii      pair<int, int>
+# define fi       first
+# define se       second
+
+// Vector
+# define pb         push_back
+# define All(v)     v.begin(), v.end()
+# define AllR(v)    v.rbegin(), v.rend()
+# define Rev(v)     reverse(All(v))
+# define Sort(v)    sort(All(v))
+# define SortR(v)   sort(AllR(v))
+
+int binex(int a, int b) {
+    if (b == 0) return 1;
+    if (b == 1) return a;
+
+    int res = binex(a, b / 2);
+    res = ((res % mod) * (res % mod)) % mod;
+
+    if (b % 2 == 1) res = ((res % mod) * (a % mod)) % mod;
+
+    return res;
+}
+
+signed main() {
+    GRIND
+    
+    int n;  cin >> n;
+    cout << binex(2, n);
+}
